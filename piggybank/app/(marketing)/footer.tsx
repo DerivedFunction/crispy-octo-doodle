06a886c0ec476a89b1865e7a2359fda1c6e6343c
Denny Li <ytdli08@gmail.com>
2025-03-06 14:50:56 -0500

    update 11

--- piggybank/app/(marketing)/footer.tsx
+++ piggybank/app/(marketing)/footer.tsx
@@ -1,19 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
       <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
-        <Button size="lg" variant="ghost" className="w-full">
-          <Image
-            src="/spendingTopic.webp"
-            alt="spending"
-            height={32}
-            width={32}
-          />
-          Spending
-        </Button>
+        <h1>CSB 395 2025</h1>
       </div>
     </footer>
   );
