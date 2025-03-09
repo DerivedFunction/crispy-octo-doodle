84a94ef8dd9ade5fdca544a187b77703333329ef
Denny Li <ytdli08@gmail.com>
2025-03-08 20:49:59 -0500

    added sidebar to pages

--- piggybank/app/pages/layout.tsx
+++ piggybank/app/pages/layout.tsx
@@ -2,17 +2,17 @@ import type { PropsWithChildren } from "react";
 
 import { Footer } from "./footer";
 import { Header } from "./header";
+import { MobileHeader } from "@/components/mobile-header";
+import { Sidebar } from "@/components/sidebar";
 
 const MarketingLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
-
-      <main className="flex flex-1 flex-col items-center justify-center">
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
+      <main className="flex flex-1 flex-col items-center justify-center pt-1">
         {children}
       </main>
-
-      <Footer />
     </div>
   );
 };
