c31e9f31e1258231a1bc5a9ab51921d4daa7568a
Denny Li <ytdli08@gmail.com>
2025-04-14 10:14:55 -0400

    replaced unit 1 content

--- piggybank/pageContent/savings/unit1.tsx
+++ piggybank/pageContent/savings/unit1.tsx
@@ -11,23 +11,16 @@ export default function content() {
 
         <div className="flex flex-col items-center gap-y-8">
           <h1 className="text-xl lg:text-3xl font-bold text-neutral-500 max-w-[480px] text-center">
-            Unit 1 Saving
+            Unit 1: Financial Planning Basics
           </h1>
 
           <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-              gravida, urna ullamcorper rutrum auctor, augue tortor luctus dui,
-              sed auctor neque tortor sed ex. Quisque semper turpis turpis, id
-              condimentum ligula tincidunt non. Aenean condimentum lorem ut
-              ultricies auctor. Curabitur porttitor mauris nec diam accumsan,
-              sit amet blandit lacus tristique. Nullam eu massa nisi. Donec
-              rhoncus malesuada rhoncus. Etiam elementum pretium tortor, nec
-              dictum mauris. Nam quam enim, bibendum et ornare vitae, mattis a
-              nisl. Duis vel sem ac mauris placerat suscipit non id eros. Cras
-              tellus eros, feugiat in risus eu, pretium dapibus nisl. Orci
-              varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              Your own goals and the things you do affect your money. To use
+              your money in the best way, you should make a money plan. This
+              plan has five simple steps: deciding what you want, consider
+              alternatives, making a plan, starting it, and keeping an eye on
+              how it&apos;s going.
             </p>
           </div>
         </div>
@@ -35,45 +28,68 @@ export default function content() {
       <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
         <div className="flex flex-col items-center gap-y-8">
           <h1 className="text-xl lg:text-3xl font-bold text-neutral-500 max-w-[480px] text-center">
-            Header
+            Step 1: Decide What You Want
           </h1>
 
           <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-              gravida, urna ullamcorper rutrum auctor, augue tortor luctus dui,
-              sed auctor neque tortor sed ex. Quisque semper turpis turpis, id
-              condimentum ligula tincidunt non. Aenean condimentum lorem ut
-              ultricies auctor. Curabitur porttitor mauris nec diam accumsan,
-              sit amet blandit lacus tristique. Nullam eu massa nisi. Donec
-              rhoncus malesuada rhoncus. Etiam elementum pretium tortor, nec
-              dictum mauris. Nam quam enim, bibendum et ornare vitae, mattis a
-              nisl. Duis vel sem ac mauris placerat suscipit non id eros. Cras
-              tellus eros, feugiat in risus eu, pretium dapibus nisl. Orci
-              varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              First, think about what you really need. Let&apos;s say you want a
+              bike because you need a way to get around. We get them because
+              they&apos;re handy, help us get to school or to meet up with
+              friends, or just for fun. But money-wise, they&apos;re something
+              we spend on. They lose value over time—they don&apos;t grow like
+              money in a piggy bank. So, getting a bike might slow down how fast
+              you can save up for something big, like a new iPhone. Plus, bikes
+              need cash for stuff like fixing them, new tires, or even a lock.
+              Keep all this in mind while you&apos;re making your plan!
             </p>
           </div>
         </div>
         <div className="flex flex-col items-center gap-y-8">
           <h1 className="text-xl lg:text-3xl font-bold text-neutral-500 max-w-[480px] text-center">
-            Header2
+            Step 2: Consider Alternatives
           </h1>
 
           <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-              gravida, urna ullamcorper rutrum auctor, augue tortor luctus dui,
-              sed auctor neque tortor sed ex. Quisque semper turpis turpis, id
-              condimentum ligula tincidunt non. Aenean condimentum lorem ut
-              ultricies auctor. Curabitur porttitor mauris nec diam accumsan,
-              sit amet blandit lacus tristique. Nullam eu massa nisi. Donec
-              rhoncus malesuada rhoncus. Etiam elementum pretium tortor, nec
-              dictum mauris. Nam quam enim, bibendum et ornare vitae, mattis a
-              nisl. Duis vel sem ac mauris placerat suscipit non id eros. Cras
-              tellus eros, feugiat in risus eu, pretium dapibus nisl. Orci
-              varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
+              Ask yourself, do you really need that new bike or the PS5? Before
+              you buy one, think about other options. Could you walk, take the
+              bus, or maybe even skateboard instead? Does your old gaming
+              console still work? Sometimes, one thing you want can affect
+              something else you&apos;re working toward. Making smart choices
+              now can help you reach bigger goals later.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+        <div className="flex flex-col items-center gap-y-8">
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-500 max-w-[480px] text-center">
+            Step 3: Make the Plan
+          </h1>
+
+          <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+            <p>
+              Write down your goal, the item you want to buy, how much it costs,
+              and about how long you think it will take to reach that goal.
+              Maybe you will realize that it may be unrealistic to save up for a
+              super expensive gaming console in just one month if you only get a
+              small allowance! Break big goals into smaller, achievable steps.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-y-8">
+          <h1 className="text-xl lg:text-3xl font-bold text-neutral-500 max-w-[480px] text-center">
+            Steps 4 & 5: Start and Monitor
+          </h1>
+
+          <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+            <p>
+              Once you&apos;ve made the plan, it&apos;s time to work toward
+              achieving that goal. Track your progress regularly and adjust if
+              needed. Remember to celebrate small wins along the way! Financial
+              planning is about making smart choices today that will help you
+              achieve what&apos;s important to you tomorrow.
             </p>
           </div>
         </div>
